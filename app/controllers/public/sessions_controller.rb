bac09b0262de16d3cf6ddd7cafc2264df119f51a
EC2 Default User <ec2-user@ip-172-31-18-54.ap-northeast-1.compute.internal>
2022-11-19 11:56:53 +0000

    新規登録、ログイン

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,5 +24,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   def after_sign_in_path_for(resource)
+    adout_path
+   end
+   
+    def after_sign_out_path_for(resource)
+    root_path
+    end
+
+   protected
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+   end
+
 end
