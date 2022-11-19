30db0ca3f612cb3508c5b347fffa849e5831a22a
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-11-19 06:00:28 +0000

    Revert "Merge pull request #5 from webcamp-member-natto/top"
    This reverts commit 4d18be42d4cf778f4fa23bfafd37ac56bdfaa930, reversing
    changes made to 3dfd981f1ea9192208b15b15262ff86656b7fc09.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  # 顧客用
-  # URL /customers/sign_in ...
-devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-  # 管理者用
-  # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
+  devise_for :customers
   root to: 'homes#top'
   get 'about'=>"homes#about",as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20221116051728_devise_create_customers.rb
+++ db/migrate/20221116051728_devise_create_customers.rb
@@ -6,14 +6,6 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :last_name, null: false, default: ""
-      t.string :first_name, null: false, default: ""
-      t.string :last_name_kana, null: false, default: ""
-      t.string :first_name_kana, null: false, default: ""
-      t.string :postal_code, null: false, default: ""
-      t.string :address, null: false, default: ""
-      t.string :phone_number, null: false, default: ""
-      t.boolean :is_deleted, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
