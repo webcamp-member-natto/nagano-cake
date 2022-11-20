47c4427af341962de86be12bfa400cd8e04fc266
EC2 Default User <ec2-user@ip-172-31-18-54.ap-northeast-1.compute.internal>
2022-11-20 02:27:17 +0000

    新規商品登録

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-  
+ has_one_attached :image
+ belongs_to :genre, optional: true
+ 
+ validates :image, presence: true
+ validates :name, presence: true
+ validates :detail, presence: true
+ validates :price, presence: true
+ 
+ 
 end
