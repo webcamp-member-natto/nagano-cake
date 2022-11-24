ab63d2d55cc42e44fa67ba68b0fbaef36c085caf
EC2 Default User <ec2-user@ip-172-31-45-177.ap-northeast-1.compute.internal>
2022-11-24 06:40:03 +0000

    11/24

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -50,7 +50,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
-    @orders = Order.all
+    @orders = current_customer.orders.all
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
  belongs_to :genre, optional: true
 
  has_many :cart_items
+ has_many :order_items
 
 
  validates :image, presence: true
