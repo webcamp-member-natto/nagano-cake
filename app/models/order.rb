033f8188f1dd69bd2b3817b0378dffb86bb02239
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-11-24 13:18:13 +0000

    detail

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,10 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
 
   belongs_to :customer
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
 
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
+
 end
