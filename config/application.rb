1eff73f9409bf79ef3309059e9821ae8a2e02d2b
akarimatsuoka <114091227+achan-m@users.noreply.github.com>
2022-11-27 13:38:46 +0000

    Complete

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module NaganoCake
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
